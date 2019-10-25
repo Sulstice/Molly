dca6191fbd0d6cc808db00bdec8807e9e90aa3d2
Sulstice <sharifsuliman1@gmail.com>
2019-10-25 01:52:08 +0100

    ss/MASTER first pass at flask

--- CaddChat.py
+++ CaddChat.py
@@ -64,4 +64,5 @@ def send_message(recipient_id, response):
     return "success"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
